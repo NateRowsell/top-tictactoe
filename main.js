b237f66b6694785abe0b969aa92819442fc1a85d
NateRowsell <nsr.72@hotmail.com>
2022-03-18 21:01:14 -0300

    gameboard object, resetButton done

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const resetButton = document.getElementById('resetButton')
 const gameModeSlider = document.getElementById('gameModeSlider')
 const gameModeCheckbox = document.getElementById('gameModeCheckbox')
 const playAsSlider = document.getElementById('playAsSlider')
@@ -9,6 +10,7 @@ X.innerText = 'clear'
 const O = document.createElement('span')
 O.classList.add('material-icons')
 O.innerText = 'radio_button_unchecked'
+const boardSpaces = document.querySelectorAll('.boardSpace')
 let bottomControlPanel = document.createElement('div')
 bottomControlPanel.setAttribute('id', 'bottomControlPanel')
 let difficultyDiv = document.createElement('div')
@@ -33,7 +35,6 @@ difficultyDiv.append(
 bottomControlPanel.appendChild(difficultyDiv)
 
 // Show difficulty when Player V Comp selected
-
 if (gameModeCheckbox.checked == true) {
   controlPanel.appendChild(bottomControlPanel)
 }
@@ -47,40 +48,56 @@ gameModeSlider.addEventListener('click', () => {
   }
 })
 
-// Find play as value x or o
-
-function getPlayAsValue() {
-  if (playAsCheckbox.checked == true) {
-    return X
-  } else {
-    return O
-  }
-}
-
 // set reset button funcionality
+resetButton.addEventListener('click', () => {
+  Gameboard.newGameboard()
+})
 
-function reset() {
-  // do after
-}
+//GAMEBOARD MODULE
+const Gameboard = (() => {
+  const topLeftSpace = document.getElementById('top-left')
+  const topMiddleSpace = document.getElementById('top-middle')
+  const topRightSpace = document.getElementById('top-right')
+  const middleLeftSpace = document.getElementById('middle-left')
+  const centerSpace = document.getElementById('center')
+  const middleRightSpace = document.getElementById('middle-right')
+  const bottomLeftSpace = document.getElementById('bottom-left')
+  const bottomMiddleSpace = document.getElementById('bottom-middle')
+  const bottomRightSpace = document.getElementById('bottom-right')
 
-// let gameBoard = {}
-// gameBoard.currentGame = new Array()
-// console.log(gameBoard)
+  let gameBoardArray = [
+    topLeftSpace,
+    topMiddleSpace,
+    topRightSpace,
+    middleLeftSpace,
+    centerSpace,
+    middleRightSpace,
+    bottomLeftSpace,
+    bottomMiddleSpace,
+    bottomRightSpace,
+  ]
 
-// const TicTacToe = () => {
-//   const gameBoard = {}
-//   const newGame = () => {
-//     gameBoard.gameboard = new Array()
-//     return gameBoard
-//   }
-//   return { newGame, gameBoard }
-// }
+  let gameBoardObject = { currentGameboard: gameBoardArray }
 
-// console.log(TicTacToe.gameBoard)
-// console.log(TicTacToe.newGame)
+  const newGameboard = () => {
+    for (let i = 0; i < gameBoardObject.currentGameboard.length; i++) {
+      if (gameBoardObject.currentGameboard[i] == null) {
+        continue
+      }
+      while (gameBoardObject.currentGameboard[i].firstChild) {
+        gameBoardObject.currentGameboard[i].removeChild(
+          gameBoardObject.currentGameboard[i].lastChild,
+        )
+      }
+    }
+  }
+  return { newGameboard, gameBoardObject }
+})()
 
+// PLAYER FACTORY
 //Player will need playas() and score()
-const Player = () => {
+const Player = (name) => {
+  let playerName = name
   let playAs = () => {
     if (playAsCheckbox.checked == true) {
       return O
@@ -89,18 +106,55 @@ const Player = () => {
     }
   }
 
-  return { playAs }
+  // determine player status first or second x or o
+
+  const playerStatus = () => {
+    if (playAs() == X) {
+      return 'firstPlayer'
+    } else {
+      return 'secondPlayer'
+    }
+  }
+
+  let makeMove = () => {
+    for (
+      let i = 0;
+      i < Gameboard.gameBoardObject.currentGameboard.length;
+      i++
+    ) {
+      Gameboard.gameBoardObject.currentGameboard[i].addEventListener(
+        'click',
+        (e) => {
+          targetSquare = e.currentTarget
+          targetSquare.appendChild(playAs())
+          // figure out below how to remove listener after one use
+          Gameboard.gameBoardObject.currentGameboard[i].removeEventListener(
+            click,
+            this,
+          )
+        },
+      )
+    }
+  }
+
+  // Need board spaces to have event listener to return targeted node
+  // Append play as function which is x or o as make move function
+
+  // for (let i = 0; i < boardSpaces.length; i++) {
+  //   boardSpaces[i].addEventListener('click', (e) => {
+  //     targetSquare = e.currentTarget
+  //     if (targetSquare.childNodes.length == 0) {
+  //       targetSquare.appendChild(playAs())
+  //     }
+  //   })
+  // }
+
+  return { playerName, playAs, playerStatus, makeMove }
 }
 
 // TESTING AREA
 // testing for adding divs with factory function
 
 let playerOne = Player()
-
-console.log(playerOne.playAs())
-
-const topLeft = document.getElementById('top-left')
-
-topLeft.appendChild(playerOne.playAs())
 
 // TESTING AREA
